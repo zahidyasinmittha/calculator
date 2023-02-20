1a03ca04740130e9690109f82f0fbed38779adb4
zahid yasin <b.zahidmittha@gmail.com>
2023-02-20 17:30:27 +0500

    all three files uploaded in working condition v1
    v1

---
+++ index.js
@@ -0,0 +1,39 @@
+let exp="";
+let d_exp="";
+let ans="";
+let inputElement = document.querySelector('input');
+let btn=document.querySelectorAll('button');
+Array.from(btn).forEach((btn)=>{
+btn.addEventListener('click',(e)=>{
+    if(e.target.innerHTML=="="){
+        try{
+        exp=eval(exp);
+        ans=exp;
+        d_exp=exp;
+        }
+        catch(err){
+            alert(err)
+        }
+    }
+    else if(e.target.innerHTML=="A"){
+        d_exp+=ans;
+    }
+    else if(e.target.innerHTML=="X"){
+        d_exp+=e.target.innerHTML;
+        exp+="*";
+    }
+    else if(e.target.innerHTML=="C"){
+        exp="";
+        d_exp=""
+    }
+    else if(e.target.innerHTML=="&lt;"){
+        exp=exp.slice(0,-1);
+        d_exp=d_exp.slice(0,-1);
+    }
+    else{
+    exp+=e.target.innerHTML;
+    d_exp+=e.target.innerHTML;
+    }
+    inputElement.value=d_exp;
+})
+})
